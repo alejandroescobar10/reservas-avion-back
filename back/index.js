2e15e1b15dca8fc403f70e927fbeb11e8857e042
alejandroescobar10 <160554099+alejandroescobar10@users.noreply.github.com>
2024-05-15 13:24:11 -0500

    se hace prueba del back en vercel

--- back/index.js
+++ back/index.js
@@ -17,3 +17,19 @@ connectDB();
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
 })
+
+//pega esto en tu archivo index del back y subis los cambios 
+
+app.get("/", (req, res) => {
+  const htmlResponse = `
+    <html>
+      <head>
+        <title>NodeJs y Express en Vercel</title>
+      </head>
+      <body>
+        <h1>Soy un proyecto Back end en vercel</h1>
+      </body>
+    </html>
+  `;
+  res.send(htmlResponse);
+});
